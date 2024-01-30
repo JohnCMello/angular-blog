b3218a9aa87ad207c6024b540b8fa88acd4b82e9
john-mello_valtech <john.mello@valtech.com>
2024-01-30 17:44:15 -0300

    feat: add rout to single post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PostDetailComponent } from '@pages/post-detail/post-detail.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
-  // {
-  //   path: 'teste',
-  //   component: NavbarComponent,
-  // },
+  {
+    path: 'posts/:id',
+    component: PostDetailComponent,
+  },
 ];
